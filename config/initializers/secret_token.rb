06af4d5a47a7b37c3ffee3a2662664d202d896ee
unknown <KIESJL@.energy.power.corp>
2012-04-02 16:03:54 -0400

    ?

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-GDP::Application.config.secret_token = '651938603e30708e3a2d03b4985e2fb8e1ca2781bc078bdf05c6ad637a8fbcf8326bf2964330337394f7818ea9c07a777c7402d11c588119302fec7bcf4cd9d3'
--- db/migrate/20120301233435_add_ticket_link_to_shows.rb
+++
@@ -1,9 +0,0 @@
-class AddTicketLinkToShows < ActiveRecord::Migration
-  def self.up
-    add_column :shows, :ticket_link, :string
-  end
-
-  def self.down
-    remove_column :shows, :ticket_link
-  end
-end
