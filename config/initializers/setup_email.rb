8a69d649de68df1b13803d25466aa25848fe05a2
James Kiesel <james.kiesel@gmail.com>
2013-11-19 18:17:01 -0500

    Remove unused initializers

--- config/initializers/setup_email.rb
+++
@@ -1,4 +0,0 @@
-  
-email_settings = YAML::load(File.open("#{Rails.root.to_s}/config/email.yml"))
-ActionMailer::Base.delivery_method = :smtp
-ActionMailer::Base.smtp_settings = email_settings[Rails.env] unless email_settings[Rails.env].nil?
