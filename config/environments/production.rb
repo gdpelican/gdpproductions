a69a42ed5fe2fe3fa5f67a3a0eb69666e6f8a59e
James Kiesel <james.kiesel@gmail.com>
2013-11-19 18:48:37 -0500

    Friggen Heroku on precompile BS

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,8 +20,6 @@ GDP::Application.configure do
   # Generate digests for assets URLs
   config.assets.digest = true
   
-  config.assets.initialize_on_precompile = false
-
   # Defaults to Rails.root.join("public/assets")
   # config.assets.manifest = YOUR_PATH
 
