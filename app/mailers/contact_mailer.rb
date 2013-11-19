86fe3d1fb4f7d477a474ed4340ffbab1a2ca0b2b
James Kiesel <james.kiesel@gmail.com>
2013-11-19 18:09:18 -0500

    Remove unused contact files

--- app/mailers/contact_mailer.rb
+++
@@ -1,8 +0,0 @@
-class ContactMailer < ActionMailer::Base
-  default to: 'contact@gdpproductions.com'
-  
-  def contact_email(message)
-    @message = message
-    mail(from: message.email, subject: 'New message from #{message.name}')
-  end
-end
