7c82d5bfd65f1af662f1749f0ef1d3361529def8
James Kiesel <james.kiesel@gmail.com>
2014-09-04 19:05:40 -0400

    Fix socials S3

--- app/models/social.rb
+++ app/models/social.rb
@@ -5,11 +5,6 @@ class Social < ActiveRecord::Base
   validates_presence_of :link
   validates_attachment_presence :icon
 
-  has_attached_file :icon,
-     :styles => {:icon => "100x100#"},
-     :storage => :s3,
-     :s3_credentials => "#{Rails.root}/config/s3.yml",
-     :path => '/:style/:id/:filename',
-     :bucket => 'GDProdSocials-TEST'
+  has_attached_file :icon, :styles => {:icon => "100x100#"}
   
 end
